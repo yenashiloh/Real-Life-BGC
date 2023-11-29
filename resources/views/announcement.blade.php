39e01926309dfc23352610dfe9fde265e1fc7bb2
CodeAce36 <paulangelo.derige@gmail.com>
2023-11-29 12:06:34 +0800

    announcement update

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -5,7 +5,7 @@
 <section id="contact" class="contact">
     <div class="container">
         <div class="section-title">
-            <h2>ANNOUNCEMENT</h2>
+            <h2>ANNOUNCEMENTS</h2>
         </div>
         <div class="container ">
             <div class="card shadow-sm" style="margin-left: 90px; margin-right: 90px;">
